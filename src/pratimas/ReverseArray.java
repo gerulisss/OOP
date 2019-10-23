53f72b96f05f90d36f5958705ecd26179eb16fec
RokisM <gerulisss@users.noreply.github.com>
2019-10-23 12:26:36 +0300

    reverse array text update

--- src/pratimas/ReverseArray.java
+++ src/pratimas/ReverseArray.java
@@ -1,4 +1,22 @@
 package pratimas;
 
-public class ReverseArray {
+import java.lang.*;
+
+class ReverseArray
+{
+    public static void main(String[] args)
+    {
+        String input = "Mokausi programuoti Java programavimo kalba";
+
+        byte [] strAsByteArray = input.getBytes();
+
+        byte [] result =
+                new byte [strAsByteArray.length];
+
+        for (int i = 0; i<strAsByteArray.length; i++)
+            result[i] =
+                    strAsByteArray[strAsByteArray.length-i-1];
+
+        System.out.println(new String(result));
+    }
 }
